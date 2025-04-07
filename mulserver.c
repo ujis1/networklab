48c1f0b0524dd0a2936deafdb15e4c544d29488b
Adhithyan H Nair <133254493+adhithyan-h-nair@users.noreply.github.com>
2025-04-07 20:34:22 +0530

    Update mulserver.c
    bro working server  created by chatGPT. mattethu work aayilla

--- mulserver.c
+++ mulserver.c
@@ -1,79 +1,94 @@
 #include <stdio.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>  // For close()
-
+#include <unistd.h> // read(), write(), close()
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
 
-// Function to handle communication between server and client
-void func(int sockfd, struct sockaddr_in *cliaddr, socklen_t len) {
-    char buff[MAX];
-    int n;
+// Function to handle communication
+void func(int connfd)
+{
+	char buff[MAX];
+	int n;
 
-    for (;;) {
-        bzero(buff, sizeof(buff));
+	for (;;) {
+		bzero(buff, MAX);
 
-        // Receive message from client
-        n = recvfrom(sockfd, buff, sizeof(buff), 0, (SA *)cliaddr, &len);
-        if (n < 0) {
-            printf("Failed to receive message\n");
-            break;
-        }
+		// Read message from client
+		read(connfd, buff, sizeof(buff));
+		printf("From client: %s\tTo client: ", buff);
 
-        printf("From client: %s\t To client: ", buff);
+		// If client sent "exit", break loop
+		if (strncmp("exit", buff, 4) == 0) {
+			printf("Server Exit...\n");
+			break;
+		}
 
-        bzero(buff, sizeof(buff));
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n');  // Get server's response
+		// Get server input to send back
+		bzero(buff, MAX);
+		n = 0;
+		while ((buff[n++] = getchar()) != '\n')
+			;
 
-        // Send response back to the client
-        sendto(sockfd, buff, sizeof(buff), 0, (SA *)cliaddr, len);
-
-        // If "exit" is typed, break the loop
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
+		// Send to client
+		write(connfd, buff, sizeof(buff));
+	}
 }
 
-int main() {
-    int sockfd;
-    struct sockaddr_in servaddr, cliaddr;
-    socklen_t len = sizeof(cliaddr);
+int main()
+{
+	int sockfd, connfd;
+	socklen_t len;
+	struct sockaddr_in servaddr, cli;
 
-    // Create UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        printf("Socket creation failed...\n");
-        exit(0);
-    } else {
-        printf("Socket successfully created..\n");
-    }
+	// Create socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
 
-    bzero(&servaddr, sizeof(servaddr));
+	bzero(&servaddr, sizeof(servaddr));
 
-    // Setup server address
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);  // Listen on any network interface
-    servaddr.sin_port = htons(PORT);
+	// Assign IP and port
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
 
-    // Bind the socket to the address and port
-    if (bind(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0) {
-        printf("Socket bind failed...\n");
-        exit(0);
-    } else {
-        printf("Socket successfully binded..\n");
-    }
+	// Bind socket
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+		printf("socket bind failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully binded..\n");
 
-    // Handle communication with client
-    func(sockfd, &cliaddr, len);
+	// Listen for client
+	if ((listen(sockfd, 5)) != 0) {
+		printf("Listen failed...\n");
+		exit(0);
+	}
+	else
+		printf("Server listening..\n");
 
-    // Close the socket after communication ends
-    close(sockfd);
+	len = sizeof(cli);
+
+	// Accept client
+	connfd = accept(sockfd, (SA*)&cli, &len);
+	if (connfd < 0) {
+		printf("server accept failed...\n");
+		exit(0);
+	}
+	else
+		printf("server accepted the client...\n");
+
+	// Start chat
+	func(connfd);
+
+	// Close socket
+	close(sockfd);
 }
-
